73f254e3476122feca7093d6ffb7bd6f8552c808
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-09-02 18:33:18 -0700

    Creating OOJS in MVC format

--- javascripts/crontab.js
+++ javascripts/crontab.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-  bindEvents();
+    var selectedMonths = [];
+    controller.bindEvents();
 })
+
+Controller = function(){};
+View = function(){};
+
+Controller.prototype = {
+
+    bindEvents: function bindEvents(){
+        $('.months label').mouseup(function(){
+            var chosenMonthBtn = this;
+            setTimeout(function(){
+                controller.recordMonth(chosenMonthBtn);
+            }, 50)
+        })
+    },
+
+    recordMonth: function(chosenMonthBtn){
+        if ($(chosenMonthBtn).hasClass('active')) {
+            var input = $(chosenMonthBtn).find('input')
+            var inputVal = $(input).attr('value')
+            console.log(inputVal)
+        }
+        else {
+            console.log('poo')
+        }
+    }
+
+}
+
+
+controller = new Controller();
+view = new View();
